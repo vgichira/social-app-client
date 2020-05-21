7bf96e3495c640695f5fa9ddee5dc2fafb451427
Vincent Gichira <vgichira39@gmail.com>
2020-05-21 17:24:12 +0300

    added login logic

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,6 +5,8 @@ import AppLogo from "../images/icon.png";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { Link } from "react-router-dom"
 import axios from "axios";
 
 import Grid from "@material-ui/core/Grid";
@@ -18,6 +20,13 @@ const styles = {
     },
     textField: {
         marginBottom: "30px"
+    },
+    pageTitle: {
+        marginBottom: "20px"
+    },
+    errorMsg: {
+        color: "red",
+        textAlign: "left"
     }
 }
 
@@ -40,10 +49,20 @@ class login extends Component {
             email: this.state.email, 
             password: this.state.password 
         }
+        try{
+            const response = await axios.post('/login', loginData);
 
-        const response = await axios.post('/login', loginData);
+            this.setState({
+                loading: false 
+            })
 
-        console.log(response.data);
+            this.props.history.push("/");
+        }catch(err){
+            this.setState({
+                errors: err.response.data, 
+                loading: false, 
+            })
+        }
     }
 
     handleChange = (event)=>{
@@ -53,8 +72,8 @@ class login extends Component {
     }
 
     render() {
-        const { classes } = this.props
-
+        const { classes } = this.props;
+        const { errors: {email, password, general}, loading} = this.state;
         return (
             <Grid container className={classes.form}>
                 <Grid item sm/>
@@ -63,16 +82,32 @@ class login extends Component {
                     <Typography variant="h4" className={classes.pageTitle}>Login</Typography>
 
                     <form noValidate onSubmit={this.handleSubmit}>
+                        {general && (
+                            <Typography className={classes.errorMsg} variant="body1">{general}</Typography>
+                        )}
                         <TextField id="email" type="email" name="email" label="Email" 
                         className={classes.textField} value={this.state.email}
+                        helperText={email}
+                        error={email ? true : false }
                         onChange={this.handleChange} fullWidth />
 
                         <TextField id="password" type="password" name="password" label="Password" 
                         className={classes.textField} value={this.state.password}
+                        helperText={password}
+                        error={password ? true : false}
                         onChange={this.handleChange} fullWidth />
-
-                        <Button variant="contained" type="submit" className={classes.button} color="primary">Login</Button>
-                    </form>
+                        
+                            <Button variant="contained" size="large" fullWidth type="submit" 
+                            className={classes.button} color="primary" disabled={loading}>
+                            {loading ? (
+                                <CircularProgress size={30} />
+                            ) : 
+                            (
+                                "Login"
+                            )}
+                            </Button>
+                    </form><br/>
+                    <small>Dont have an account? <Link to="/signup">Create Free Account</Link></small>
                 </Grid>
                 <Grid item sm/>
             </Grid>
